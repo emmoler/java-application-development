9f896d62a218ce3c8dbd686466fbf8ed3ca3d9f6
Oleg <emmoler@yandex.ru>
2022-04-21 19:07:43 +0300

    refactoring

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -8,108 +8,101 @@ import static java.lang.System.lineSeparator;
 
 public class Facade {
 
-    private static final String NO_INPUT = "NO_INPUT";
+    private static final String NO_NEW_INPUT = "";
 
     private static int ACCUMULATOR_INT = 0;
     private static int ACCUMULATOR_STRING_COUNT = 0;
     private static String ACCUMULATOR_LAST_STRING = "";
-    private static String PREVIOUS_JAVA_LANG_TYPE = "";
     private static String CONCATENATED_LOGGER_CONTENT = "";
 
-    private static LoggerState LOGGER_STATE;
-    private enum LoggerState {
-        IntegerAccumulation,
-        ByteAccumulation,
-        StringAccumulation
+    private static Object currentLogObject;
+
+    private static AccumulationType ACCUMULATION_TYPE = AccumulationType.Other;
+    private enum AccumulationType {
+        Integer,
+        Byte,
+        String,
+        Other
     }
 
-    public static void log(Object obj) { appendLogValue(obj); }
-    public static void flush() { print(getLoggerContent()); }
-
-    public static String getLoggerContent() {
-        appendLogValue(null, NO_INPUT);
-        String loggerContent = CONCATENATED_LOGGER_CONTENT;
-        clearMemoryLoggerContent();
-        clearMemoryInt();
-        clearMemoryString();
-        return loggerContent;
+    public static void log(Object obj) {
+        currentLogObject = obj;
+        appendAccumulatedLines();
+        accumulateAdditionalLine();
+        appendCommonLogLine();
     }
 
-    public static void appendLogValue(Object obj) {
-        appendLogValue(obj, obj.getClass().getName());
+    public static void flush() {
+        currentLogObject = null;
+        appendAccumulatedLines();
+        clearMemory();
     }
 
-    private static void appendLogValue(Object obj, String currentLogObjType) {
-
-        if (PREVIOUS_JAVA_LANG_TYPE.equals(JAVA_LANG_INTEGER) & !currentLogObjType.equals(JAVA_LANG_INTEGER)) {
-            appendIntegerLogLine();
-            clearMemoryInt();
-        }
-
-        if (PREVIOUS_JAVA_LANG_TYPE.equals(JAVA_LANG_BYTE) & !currentLogObjType.equals(JAVA_LANG_BYTE)) {
-            appendIntegerLogLine();
-            clearMemoryInt();
-        }
-
-        if ( (PREVIOUS_JAVA_LANG_TYPE.equals(JAVA_LANG_STRING) & !currentLogObjType.equals(JAVA_LANG_STRING)) ||
-             (PREVIOUS_JAVA_LANG_TYPE.equals(JAVA_LANG_STRING) & currentLogObjType.equals(JAVA_LANG_STRING) && !ACCUMULATOR_LAST_STRING.equals(obj.toString()))) {
-            appendStringLogLine();
-            clearMemoryString();
-        }
-
-        PREVIOUS_JAVA_LANG_TYPE = currentLogObjType;
-
-        switch (currentLogObjType)
+    private static void accumulateAdditionalLine() {
+        switch (currentLogObject.getClass().getName())
         {
             case JAVA_LANG_INTEGER:
-                accumulateNewIntegerOrOverflowString((int)obj);
+                accumulateNewIntegerOrOverflowString((int)currentLogObject);
                 break;
             case JAVA_LANG_BYTE:
-                accumulateNewByteOrOverflowString((byte)obj);
+                accumulateNewByteOrOverflowString((byte)currentLogObject);
                 break;
             case JAVA_LANG_STRING:
-                accumulateNewString(obj.toString());
+                accumulateNewString(currentLogObject.toString());
                 break;
-            case NO_INPUT:
-                break;
-            default:
-                appendCommonLogLine(obj);
+        }
+    }
+
+    private static void appendAccumulatedLines() {
+        String currentLogObjType = NO_NEW_INPUT;
+        if (!(currentLogObject ==null)) currentLogObjType = currentLogObject.getClass().getName();
+
+        if (ACCUMULATION_TYPE == AccumulationType.Integer & !currentLogObjType.equals(JAVA_LANG_INTEGER)) {
+            appendIntegerLogLine();
+        }
+
+        if (ACCUMULATION_TYPE == AccumulationType.Byte & !currentLogObjType.equals(JAVA_LANG_BYTE)) {
+            appendByteLogLine();
+        }
+
+        if (ACCUMULATION_TYPE == AccumulationType.String & !currentLogObjType.equals(JAVA_LANG_STRING))  {
+            appendStringLogLine();
+        }
+
+        if (ACCUMULATION_TYPE == AccumulationType.String & currentLogObjType.equals(JAVA_LANG_STRING) && !ACCUMULATOR_LAST_STRING.equals(currentLogObject.toString())) {
+            appendStringLogLine();
         }
     }
 
     private static void accumulateNewString(String string) {
         ACCUMULATOR_STRING_COUNT++;
         ACCUMULATOR_LAST_STRING = string;
-        LOGGER_STATE = LoggerState.StringAccumulation;
+        ACCUMULATION_TYPE = AccumulationType.String;
     }
 
     private static void accumulateNewIntegerOrOverflowString(int newNumber) {
         if (ACCUMULATOR_INT > 0 && newNumber > Integer.MAX_VALUE - ACCUMULATOR_INT) {
-            PREVIOUS_JAVA_LANG_TYPE = JAVA_LANG_STRING;
             accumulateNewString("Integer.MAX_VALUE");
         } else if (ACCUMULATOR_INT < 0 && newNumber < Integer.MIN_VALUE - ACCUMULATOR_INT) {
-            PREVIOUS_JAVA_LANG_TYPE = JAVA_LANG_STRING;
             accumulateNewString("Integer.MIN_VALUE");
         }
         else
         {
             ACCUMULATOR_INT += newNumber;
-            LOGGER_STATE = LoggerState.IntegerAccumulation;
+            ACCUMULATION_TYPE = AccumulationType.Integer;
         }
     }
 
     private static void accumulateNewByteOrOverflowString(byte newNumber) {
         if (ACCUMULATOR_INT > 0 && newNumber > Byte.MAX_VALUE - ACCUMULATOR_INT) {
-            PREVIOUS_JAVA_LANG_TYPE = JAVA_LANG_STRING;
             accumulateNewString("Byte.MAX_VALUE");
         } else if (ACCUMULATOR_INT < 0 && newNumber < Byte.MIN_VALUE - ACCUMULATOR_INT) {
-            PREVIOUS_JAVA_LANG_TYPE = JAVA_LANG_STRING;
             accumulateNewString("Byte.MIN_VALUE");
         }
         else
         {
             ACCUMULATOR_INT += newNumber;
-            LOGGER_STATE = LoggerState.ByteAccumulation;
+            ACCUMULATION_TYPE = AccumulationType.Byte;
         }
     }
 
@@ -126,25 +119,38 @@ public class Facade {
         appendLogLine(MessageDecorator.getPrefix(ACCUMULATOR_INT) + ACCUMULATOR_INT);
     }
 
-    private static void appendCommonLogLine(Object obj) {
-        appendLogLine(MessageDecorator.getPrefix(obj) + obj.toString());
+    private static void appendByteLogLine() {
+        appendLogLine(MessageDecorator.getPrefix(ACCUMULATOR_INT) + ACCUMULATOR_INT);
+    }
+
+    private static void appendCommonLogLine() {
+        if (ACCUMULATION_TYPE == AccumulationType.Other)
+        appendLogLine(MessageDecorator.getPrefix(currentLogObject) + currentLogObject.toString());
     }
 
     private static void appendLogLine(String message) {
-        CONCATENATED_LOGGER_CONTENT = CONCATENATED_LOGGER_CONTENT + message + lineSeparator();
+        print(CONCATENATED_LOGGER_CONTENT += message + lineSeparator());
+        clearMemory();
     }
 
-    private static void clearMemoryLoggerContent() {
-        CONCATENATED_LOGGER_CONTENT = "";
-        PREVIOUS_JAVA_LANG_TYPE = "";
-    }
-
-    private static void clearMemoryString() {
-        ACCUMULATOR_LAST_STRING = "";
-        ACCUMULATOR_STRING_COUNT = 0;
-    }
-
-    private static void clearMemoryInt() {
-        ACCUMULATOR_INT = 0;
+    private static void clearMemory() {
+        switch (ACCUMULATION_TYPE)
+        {
+            case Other:
+                CONCATENATED_LOGGER_CONTENT = "";
+                ACCUMULATOR_INT = 0;
+                ACCUMULATOR_LAST_STRING = "";
+                ACCUMULATOR_STRING_COUNT = 0;
+                break;
+            case Integer:
+            case Byte:
+                ACCUMULATOR_INT = 0;
+                break;
+            case String:
+                ACCUMULATOR_LAST_STRING = "";
+                ACCUMULATOR_STRING_COUNT = 0;
+                break;
+        }
+        ACCUMULATION_TYPE = AccumulationType.Other;
     }
 }
